e723b246426ef5a73a311b21bbd2c2e5e63a7f6b
giddt <giddt@amazon.com>
2019-05-22 10:20:10 -0500

    updated for SSE-KMS with custom context

--- s3ClientSideEncryption/Program.cs
+++ s3ClientSideEncryption/Program.cs
@@ -11,6 +11,7 @@ using Amazon;
 using Amazon.S3;
 using Amazon.S3.Model;
 using Amazon.S3.Encryption;
+using Amazon.S3.Transfer;
 using Amazon.KeyManagementService;
 using Amazon.KeyManagementService.Model;
 
@@ -22,12 +23,13 @@ namespace s3ClientSideEncryption
 
         static string keyName = "NetDataEncryptionKeyForLegacyApplication";
 
-       static Amazon.RegionEndpoint defaultEndpoint = Amazon.RegionEndpoint.USEast1;
+        static Amazon.RegionEndpoint defaultEndpoint = Amazon.RegionEndpoint.USEast1;
+        //Encryption context needs to be a json string, both the key and the value can be changed
+        static string jsonStringEncryptionContext = "{\"customContext\": \"This is my context, there are many like it but this one is mine \"}";
 
 
         public static void Main(string[] args)
         {
-            string kmsKeyID = null;
             string filePath = args.Any() ? args[0] : "";
             if (string.IsNullOrEmpty(filePath) || !System.IO.File.Exists(filePath))
             {
@@ -35,30 +37,29 @@ namespace s3ClientSideEncryption
                 Console.ReadKey();
 
             }
+            UploadFileWithClientSideEncryption(filePath);
+
+        }
+
+     
+        static void UploadFileWithClientSideEncryption(string filePath)
+        {
+            string kmsKeyID = "";
+          
 
             var objectKey = System.IO.Path.GetFileName(filePath);
 
             using (var kmsClient = new AmazonKeyManagementServiceClient(defaultEndpoint))
             {
-                
-               // var response = kmsClient.CreateKeyAsync(new CreateKeyRequest()).GetAwaiter().GetResult();
 
-              var  keyData = GetKeyByAlias(keyName);
-
-                kmsKeyID = keyData;
-
-              //  var keyMetadata = keyData?.KeyMetadata; // An object that contains information about the CMK created by this operation.
-              
+             
+                kmsKeyID = GetKeyByAlias(keyName, kmsClient);
                 var kmsEncryptionMaterials = new EncryptionMaterials(kmsKeyID);
-                // CryptoStorageMode.ObjectMetadata is required for KMS EncryptionMaterials
-                var config = new AmazonS3CryptoConfiguration()
-                {
-                    StorageMode = CryptoStorageMode.ObjectMetadata
-                };
+                                  
 
-
-                using (var s3Client = new AmazonS3EncryptionClient(defaultEndpoint,  kmsEncryptionMaterials))
+                using (var s3Client = new AmazonS3EncryptionClient(defaultEndpoint, kmsEncryptionMaterials))
                 {
+
                     // encrypt and put object
                     var putRequest = new PutObjectRequest
                     {
@@ -66,7 +67,29 @@ namespace s3ClientSideEncryption
                         Key = objectKey,
                         FilePath = filePath
                     };
+
+                    //Set the master key id for the key you want to use, each object will be encrypted with its own data key
+                    putRequest.Headers["x-amz-server-side-encryption-aws-kms-key-id"] = kmsKeyID;
+                    //SET the encryption context header
+                    putRequest.Headers["x-amz-server-side-encryption-context"] = System.Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(jsonStringEncryptionContext));
+
+                    //Set the server side encryption mode to KMS
+                    putRequest.Headers["x-amz-server-side-encryption"] = "aws:kms";
+                    
+                    
+
+
                     s3Client.PutObjectAsync(putRequest).GetAwaiter().GetResult();
+                }
+                
+                // The KeyID is actually embedded in the metadata of the object and the encryptionclient automatically looks it up so you don't actually have to do that yourself
+                var kem2 = new EncryptionMaterials("1111111-11111-11111111-11111111");
+
+        
+
+                using (var s3Client2 = new AmazonS3EncryptionClient(defaultEndpoint, kem2))
+                {
+
 
                     // get object and decrypt
                     var getRequest = new GetObjectRequest
@@ -75,9 +98,10 @@ namespace s3ClientSideEncryption
                         Key = objectKey
                     };
 
-                    string fPath2 = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(filePath), System.IO.Path.GetFileNameWithoutExtension(filePath) + "_" +  new Random().Next(0, 1000).ToString() +  System.IO.Path.GetExtension(filePath));
-                    
-                   using (var getResponse = s3Client.GetObjectAsync(getRequest).GetAwaiter().GetResult())
+                    string fPath2 = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(filePath), System.IO.Path.GetFileNameWithoutExtension(filePath) + "_" + new Random().Next(0, 1000).ToString() + System.IO.Path.GetExtension(filePath));
+                    //You don't have to re-set all of the headers here because the S3EncryptionClient reads the metadata automatically and uses it to preform the decryption
+
+                    using (var getResponse = s3Client2.GetObjectAsync(getRequest).GetAwaiter().GetResult())
                     using (var stream = getResponse.ResponseStream)
                     using (var reader = new StreamReader(stream))
                     {
@@ -98,7 +122,7 @@ namespace s3ClientSideEncryption
             Console.ReadKey();
         }
 
-        static string GetKeyByAlias(string alias)
+        static string GetKeyByAlias(string alias, AmazonKeyManagementServiceClient client)
         {
             //how does it know what credentials to use? - it first looks at app.config or webconfig, 
             // then the profile in the SDK Store, then the local credentials file
@@ -109,9 +133,7 @@ namespace s3ClientSideEncryption
             // for production we would use the local store on the EC2 instance
             //this should be transparent and allow for definition by environment
 
-            var client = new AmazonKeyManagementServiceClient(defaultEndpoint);
-
-
+        
             var aliasResponse = client.ListAliasesAsync(new ListAliasesRequest() { Limit = 1000 }).GetAwaiter().GetResult();
 
             if (aliasResponse == null || aliasResponse.Aliases == null)
